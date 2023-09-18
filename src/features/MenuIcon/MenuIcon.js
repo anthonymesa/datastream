5ff27fac110658309f2e9a571151c48c1e971121
Anthony Mesa <admesa@pm.me>
2023-09-18 09:39:50 -0400

    fixed #7

--- src/features/MenuIcon/MenuIcon.js
+++ src/features/MenuIcon/MenuIcon.js
@@ -3,77 +3,77 @@ import { Transition, ActionIcon, Menu, Divider, Button } from '@mantine/core';
 import { useWindowScroll } from '@mantine/hooks';
 import { FiMenu } from 'react-icons/fi';
 import ModalActionEditSlice, { setParentId, openModal, openedSelector } from '../ModalActionAdd/ModalActionAddSlice';
-import { useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import debounce from 'lodash/debounce';
 
 function MenuIcon() {
   const [isScrolling, setIsScrolling] = useState(false);
-  const [menuVisible, setMenuVisible] = useState(false); // State to control menu visibility
-  const [scroll, scrollTo] = useWindowScroll();
   const iconRef = useRef();
-  const dispatch = useDispatch()
-  const addModalOpened = useSelector(store => store.ui.modals.actionAdd.opened)
-  const editModalOpened = useSelector(store => store.ui.modals.actionEdit.opened)
+  const dispatch = useDispatch();
+  const addModalOpened = useSelector(store => store.ui.modals.actionAdd.opened);
+  const editModalOpened = useSelector(store => store.ui.modals.actionEdit.opened);
+  const [scroll, scrollTo] = useWindowScroll();
+
+  // When scroll starts
+  useEffect(() => {
+    setIsScrolling(true);
+  }, [scroll]);
+
+  // Using debounce to detect when scrolling has stopped
+  const handleScrollEnd = debounce(() => {
+    setIsScrolling(false);
+  }, 500);
 
   useEffect(() => {
-    let timer;
-
-    setIsScrolling(true);
-    clearTimeout(timer);
-    timer = setTimeout(() => setIsScrolling(false), 200);
-    
+    handleScrollEnd();
+    // Cleanup to avoid memory leaks
     return () => {
-      clearTimeout(timer);
+      handleScrollEnd.cancel();
     };
   }, [scroll]);
-  
+
   const handleNewActionClick = () => {
-    dispatch(setParentId({ value: '' }))
-    dispatch(openModal({}))
-  }
+    dispatch(setParentId({ value: '' }));
+    dispatch(openModal({}));
+  };
 
   return (
-              <Transition
-        mounted={!isScrolling}
-        transition="translateY 1500ms ease-in-out"
-      >
-        {(transitionStyles) => (
-          <div style={{
-            ...transitionStyles,
-            position: 'fixed',
-            bottom: 0,
-            width: '100%',
-            display: "flex",
-            justifyContent: "center",
-            paddingBottom: "2rem",
-            transform: `translateY(${isScrolling || addModalOpened || editModalOpened ? '100%' : '0'})`,
-            zIndex: 999
-          }}>
-            
-    <Menu zIndex={1000} width={"50%"} position={"top"}>
-      <Menu.Target>
-
-              <ActionIcon 
-                style={
-                  {
-                    boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, .25)"
-                  }
-                }
-                color="green"size="3rem" radius="xl" variant="filled">
-                <FiMenu size="1.5rem"/>
+    <Transition
+      mounted={!isScrolling && !addModalOpened && !editModalOpened}
+      transition="slide-up"
+      duration={500}
+      timingFunction='ease'
+    >
+      {(transitionStyles) => (
+        <div style={{
+          ...transitionStyles,
+          position: 'fixed',
+          bottom: 0,
+          width: '100%',
+          display: "flex",
+          justifyContent: "center",
+          paddingBottom: "2rem",
+          zIndex: 100
+        }}>
+          <Menu zIndex={1000} width={"50%"} position={"top"}>
+            <Menu.Target>
+              <ActionIcon
+                style={{ boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, .25)" }}
+                color="green" size="3rem" radius="xl" variant="filled">
+                <FiMenu size="1.5rem" />
               </ActionIcon>
-       
-      </Menu.Target>
-      <Menu.Dropdown>
-      <Menu.Label>Create new datum...</Menu.Label>
-      <Menu.Item onClick={handleNewActionClick}>Action</Menu.Item>
-      <Divider/>
-      <Menu.Label>Misc.</Menu.Label>
-      <Menu.Item disabled>Settings</Menu.Item>
-      </Menu.Dropdown>
-      </Menu>
-   </div>
-        )}
-      </Transition>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Label>Create new datum...</Menu.Label>
+              <Menu.Item onClick={handleNewActionClick}>Action</Menu.Item>
+              <Divider />
+              <Menu.Label>Misc.</Menu.Label>
+              <Menu.Item disabled>Settings</Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
+      )}
+    </Transition>
   );
 }
 
